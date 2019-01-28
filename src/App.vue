eb25ab6bde1f5441384fadf4c4f834b650916776
Yuto Fujimura <yuto101024@gmail.com>
2019-01-28 16:24:59 +0900

    Initial commit new file:   .babelrc new file:   .editorconfig new file:   .gitignore new file:   .postcssrc.js new file:   README.md new file:   build/build.js new file:   build/check-versions.js new file:   build/logo.png new file:   build/utils.js new file:   build/vue-loader.conf.js new file:   build/webpack.base.conf.js new file:   build/webpack.dev.conf.js new file:   build/webpack.prod.conf.js new file:   config/dev.env.js new file:   config/index.js new file:   config/prod.env.js new file:   index.html new file:   package-lock.json new file:   package.json new file:   src/App.vue new file:   src/assets/logo.png new file:   src/components/HelloWorld.vue new file:   src/main.js new file:   static/.gitkeep

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <form v-on:submit.prevent="doSubmit">
+      <input type="text" v-model="data">
+      <button type="submit">test</button>
+    </form>
+    <tr v-for="item in buttons">
+      <td class="button">
+        <button v-on:click="item.func">{{ item.data }}</button>
+      </td>
+    </tr>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  methods: {
+    doSubmit: function(event, value) {
+      console.log(this.data)
+      var data = this.data
+      this.buttons.push({
+        data: data,
+        func: function() {
+          console.log(data)
+        }
+      })
+      this.data = ''
+    }
+  },
+  data () {
+    return {
+      data: '',
+      buttons: []
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
